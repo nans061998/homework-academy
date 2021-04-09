fcd1faeaf9bcad405093ba6710731efaca1adf9a
Анастасия <nastyaoseeva1998@gmail.com>
2021-04-09 16:05:20 +0700

    Доделала футер,about,testimonials

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
-$('.carousel').carousel({
-    interval: 2000
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 2,
+        speed: 1000,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 500,
+    });
 })
