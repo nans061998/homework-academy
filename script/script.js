59acb5a21cef1ccb67a81408a512c98d42583a29
Anastasia <nastyaoseeva1998@gmail.com>
2021-04-11 16:09:40 +0700

    'Адаптивность'

--- script/script.js
+++ script/script.js
@@ -2,11 +2,6 @@ $(document).ready(function(){
     $('.slider').slick({
         arrows: false,
         dots: true,
-        adaptiveHeight: true,
-        slidesToShow: 2,
-        speed: 1000,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 500,
+        slidesToShow: 3,
     });
 })
